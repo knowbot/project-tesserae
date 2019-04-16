cb5ddc1d5106a92e83f1dd701e10a34ab620f920
Andrea Trevisin <andre.trevisin@gmail.com>
2019-04-16 16:55:43 +0200

    hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_tesserae
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
